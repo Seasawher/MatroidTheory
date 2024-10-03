5a50cbd87bf958cc66b7259b0a425f72fc6ca853
Seasawher <tellagoodstory@icloud.com>
2024-10-04 03:41:30 +0900

    ビルドが成功

--- lakefile.lean
+++ lakefile.lean
@@ -4,12 +4,16 @@ open Lake DSL
 package "MatroidTheory" where
   -- Settings applied to both builds and interactive editing
   leanOptions := #[
-    ⟨`pp.unicode.fun, true⟩ -- pretty-prints `fun a ↦ b`
+    ⟨`pp.unicode.fun, true⟩, -- pretty-prints `fun a ↦ b`
+    ⟨`autoImplicit, false⟩,
+    ⟨`relaxedAutoImplicit, false⟩
   ]
   -- add any additional package configuration options here
 
-require "leanprover-community" / "mathlib"
+require mathlib from git
+  "https://github.com/leanprover-community/mathlib4.git" @ "master"
 
 @[default_target]
 lean_lib «MatroidTheory» where
   -- add any library configuration options here
+  globs := #[.submodules `MatroidTheory]
